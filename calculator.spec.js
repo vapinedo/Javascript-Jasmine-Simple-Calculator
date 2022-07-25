68b41a0c0d5ce4fc4f206edb2dc0252e52f1f127
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-25 09:18:04 -0500

    [add] test cases for calculator.js

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,3 +1,14 @@
 describe('Test Suites :: calculator.js', () => {
+    it('should add number to the total', () => {
         
+    });
+    it('should subtract number from the total', () => {
+
+    });
+    it('should multiply number with the total', () => {
+
+    });
+    it('should divide number by the total', () => {
+
+    });
 });
