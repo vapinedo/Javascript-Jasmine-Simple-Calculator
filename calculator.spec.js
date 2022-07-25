4880acb04e7e343100aa3638deb69655103c5090
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-25 18:05:32 -0500

    [add] expections for all specs items

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,10 +1,25 @@
-describe("Test Suites :: calculator.js", () => {
+describe("TEST SUITE :: calculator.js", () => {
   it("should add number to the total", () => {
     const calculator = new Calculator();
     calculator.add(5);
     expect(calculator.total).toBe(5);
   });
-  it("should subtract number from the total", () => {});
-  it("should multiply number with the total", () => {});
-  it("should divide number by the total", () => {});
+  it("should subtract number from the total", () => {
+    const calculator = new Calculator();
+    calculator.total = 30;
+    calculator.subtract(5);
+    expect(calculator.total).toBe(25);
+  });
+  it("should multiply number with the total", () => {
+    const calculator = new Calculator();
+    calculator.total = 10;
+    calculator.multiply(2);
+    expect(calculator.total).toBe(20);
+  });
+  it("should divide number by the total", () => {
+    const calculator = new Calculator();
+    calculator.total = 10;
+    calculator.divide(2);
+    expect(calculator.total).toBe(5);
+  });
 });
