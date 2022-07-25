a61fba9448e3d8073434bc0555f2d260cff62489
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-25 13:04:59 -0500

    [add] first expectation

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,14 +1,10 @@
-describe('Test Suites :: calculator.js', () => {
-    it('should add number to the total', () => {
-        
-    });
-    it('should subtract number from the total', () => {
-
-    });
-    it('should multiply number with the total', () => {
-
-    });
-    it('should divide number by the total', () => {
-
-    });
+describe("Test Suites :: calculator.js", () => {
+  it("should add number to the total", () => {
+    const calculator = new Calculator();
+    calculator.add(5);
+    expect(calculator.total).toBe(5);
+  });
+  it("should subtract number from the total", () => {});
+  it("should multiply number with the total", () => {});
+  it("should divide number by the total", () => {});
 });
