fb1e211a0944f8f5e00bba771bcef0f81dcee7b5
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-26 10:10:08 -0500

    [add] matcher toBe

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,25 +1,36 @@
-xdescribe("TEST SUITE :: calculator.js", () => {
+describe("TEST SUITE :: calculator.js", () => {
   it("should add number to the total", () => {
     const calculator = new Calculator();
     calculator.add(5);
     expect(calculator.total).toBe(5);
   });
+
   it("should subtract number from the total", () => {
     const calculator = new Calculator();
     calculator.total = 30;
     calculator.subtract(5);
     expect(calculator.total).toBe(25);
   });
+
   it("should multiply number with the total", () => {
     const calculator = new Calculator();
     calculator.total = 10;
     calculator.multiply(2);
     expect(calculator.total).toBe(20);
   });
+
   it("should divide number by the total", () => {
     const calculator = new Calculator();
     calculator.total = 10;
     calculator.divide(2);
     expect(calculator.total).toBe(5);
+  });
+
+  it("should initialize the total", () => {
+    const calculator = new Calculator();
+    expect(calculator.total).toBe(0);
+    // let player1 = { name: 'karim' };
+    // let player2 = { name: 'karim' };
+    // expect(player1).toBe(player1);
   });
 });
