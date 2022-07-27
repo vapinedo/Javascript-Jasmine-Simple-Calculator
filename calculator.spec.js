ccadead1989d75c477c81805072c1b44caf4d791
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-27 08:50:00 -0500

    [add] matcher toContain

--- calculator.spec.js
+++ calculator.spec.js
@@ -1,10 +1,12 @@
 describe("TEST SUITE :: calculator.js", () => {
+  // toBe
   it("should add number to the total", () => {
     const calculator = new Calculator();
     calculator.add(5);
     expect(calculator.total).toBe(5);
   });
 
+  // toBe
   it("should subtract number from the total", () => {
     const calculator = new Calculator();
     calculator.total = 30;
@@ -12,6 +14,7 @@ describe("TEST SUITE :: calculator.js", () => {
     expect(calculator.total).toBe(25);
   });
 
+  // toBe
   it("should multiply number with the total", () => {
     const calculator = new Calculator();
     calculator.total = 10;
@@ -19,6 +22,7 @@ describe("TEST SUITE :: calculator.js", () => {
     expect(calculator.total).toBe(20);
   });
 
+  // toBe
   it("should divide number by the total", () => {
     const calculator = new Calculator();
     calculator.total = 10;
@@ -26,23 +30,27 @@ describe("TEST SUITE :: calculator.js", () => {
     expect(calculator.total).toBe(5);
   });
 
+  // toBe
   it("should initialize the total", () => {
     const calculator = new Calculator();
     expect(calculator.total).toBe(0);
   });
 
+  // toEqual
   it("should initialize the constructor", () => {
     const calculator1 = new Calculator();
     const calculator2 = new Calculator();
     expect(calculator1).toEqual(calculator2);
   });
 
+  // not
   it("should have unique calculator object", () => {
     const calculator1 = new Calculator();
     const calculator2 = new Calculator();
     expect(calculator1).not.toBe(calculator2);
   });
 
+  // toBeDefined & toBeUndefined
   it("should have common methods", () => {
     const calculator = new Calculator();
     expect(calculator.add).not.toBeUndefined();
@@ -51,9 +59,18 @@ describe("TEST SUITE :: calculator.js", () => {
     expect(calculator.divide).not.toBeUndefined();
   });
 
+  // toBeNull
   it("can overrite total value", () => {
     const calculator = new Calculator();
     calculator.total = null;
     expect(calculator.total).toBeNull();
+  });
+
+  // toContain ( for arrays & strings )
+  it('should have the calculator constructor', () => {
+    const calculator = new Calculator();
+    let arr = [1, 2, 3, 4];
+    expect(arr).toContain(3);
+    expect(calculator.constructor.name).toContain('alc');
   });
 });
