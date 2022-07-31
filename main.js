04f63902a3250333e9fb8a21b21be98894a3d300
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-31 08:55:08 -0500

    [add] main.spec.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function calculate(event) {
   const operation = inputValue.match(regExp);
 
   if (isNaN(numberA) || isNaN(numberB) || operation === null) {
-    updateResult('Operation not recognized');
+    updateResult("Expression not recognized");
     return;
   }
 
@@ -35,16 +35,16 @@ function calculate(event) {
     case "/":
       result = calculator.divide(numberB);
       break;
+    default:
+      result = "Operation not recognized";
   }
 
   updateResult(result);
+}
 
-  function updateResult(result) {
-    const element = document.getElementById("result");
-    if (element != null) {
-      element.innerText = result;
-    }
+function updateResult(result) {
+  const element = document.getElementById("result");
+  if (element !== null) {
+    element.innerText = result;
   }
-
-  console.log({ result });
 }
