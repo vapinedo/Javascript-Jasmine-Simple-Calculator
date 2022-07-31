6595ff4495f5424a0224dbc97f0bad7fe92e1fb8
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-31 10:01:06 -0500

    [add] Implement beforeAll and afterAll lifecycle methods in main spec file

--- main.spec.js
+++ main.spec.js
@@ -1,16 +1,29 @@
 describe("TEST SUITE :: main.js", () => {
   describe("main()", () => {
-    xit("should validate the expression", () => {
-
-    });
-    xit('should call add');
-    xit('should call subtract');
-    xit('should call multiply');
-    xit('should call divide');
-    xit('should validate operation');
-    xit('should calls updateResult');
+    xit("should validate the expression", () => {});
+    xit("should call add");
+    xit("should call subtract");
+    xit("should call multiply");
+    xit("should call divide");
+    xit("should validate operation");
+    xit("should calls updateResult");
   });
   describe("updateResult()", () => {
-    xit("should add result to the DOM Element", () => {});
+    let element = null;
+    beforeAll(() => {
+      element = document.createElement("div");
+      element.setAttribute("id", "result");
+      document.body.appendChild(element);
+    });
+
+    afterAll(() => {
+      const element = document.getElementById("result");
+      document.body.removeChild(element);
+    });
+
+    it("should add result to the DOM Element", () => {
+      updateResult("5");
+      expect(element.innerText).toBe("5");
+    });
   });
 });
