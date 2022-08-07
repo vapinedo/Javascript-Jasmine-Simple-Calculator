c9ece2e9d1300927ca4059d653454ea315c91c81
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-08-07 09:10:53 -0500

    [add]  Create new getter property in calculator object using Object defineProperty

--- calculator.js
+++ calculator.js
@@ -35,3 +35,11 @@ Calculator.prototype.divide = function (number) {
   }
   return (this.total /= number);
 };
+
+Object.defineProperty(Calculator.prototype, "version", {
+  get: function() {
+    return "0.1";
+  },
+  configurable: true,
+  enumerable: true,
+});
